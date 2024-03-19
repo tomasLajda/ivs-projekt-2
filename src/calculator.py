38fa4198b50ef7bf66f321830f7a88e3d16c5a70
Martin Valapka <martin.valapka1@gmail.com>
2024-03-19 20:49:17 +0100

    Creation of digit and operator buttons

--- src/calculator.py
+++ src/calculator.py
@@ -30,6 +30,31 @@ class App(CTk):
         self.totalExpression = "0"
         self.currentExpression = "0"
 
+        self.digits = {
+            7: (1, 1),
+            8: (1, 2),
+            9: (1, 3),
+            4: (2, 1),
+            5: (2, 2),
+            6: (2, 3),
+            1: (3, 1),
+            2: (3, 2),
+            3: (3, 3),
+            0: (4, 2)
+        }
+
+        self.operations = {
+            "/": "\u00F7",
+            "*": "\u00D7",
+            "-": "-",
+            "+": "+"
+        }
+
+        self.brackets = {
+            "(": "(",
+            ")": ")"
+        }
+
     def center_window(self, width, height, scale_factor=1.0):
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
@@ -41,22 +66,43 @@ class App(CTk):
         self.displayFrame = CTkFrame(self, width=400, height=150, fg_color=DARK_GRAY, border_width=5, corner_radius=0)
         self.displayFrame.grid(row=0, column=0, sticky="nsew")
         self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        total_label = CTkLabel(self.displayFrame, text=self.totalExpression, anchor="e", padx=15, pady=15, font=(SMALL, 30))
+        total_label = CTkLabel(self.displayFrame, text=self.totalExpression, anchor="e", padx=15, pady=15,
+                               font=(SMALL, 30))
         total_label.pack(side="top", expand=True, fill="both")
 
-        current_label = CTkLabel(self.displayFrame, text=self.currentExpression, anchor="e", padx=15, pady=20, font=(LARGE, 50))
+        current_label = CTkLabel(self.displayFrame, text=self.currentExpression, anchor="e", padx=15, pady=20,
+                                 font=(LARGE, 50))
         current_label.pack(side="top", expand=True, fill="both")
 
     def create_button_frame(self):
         self.buttonFrame = CTkFrame(self, width=400, height=255, fg_color=GRAY, border_width=0, corner_radius=0)
-        self.buttonFrame.grid(row=1, column=0, sticky="nsew", rowspan=2)
+        self.buttonFrame.grid(row=1, column=0, sticky="nsew")
         self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+    def create_digit_buttons(self):
+        for digit, (row, column) in self.digits.items():
+            button = CTkButton(self.buttonFrame, text=str(digit), bg_color=GRAY, fg_color=LIGHT_GRAY,
+                               border_width=0, corner_radius=0, font=(LARGE, 25), width=75, height=45)
+            button.grid(row=row, column=column, sticky="nsew")
+
+    def create_operator_buttons(self):
+        row = 1
+        column = 4
+        for operator, symbol in self.operations.items():
+            button = CTkButton(self.buttonFrame, text=symbol, fg_color=ORANGE,
+                               border_width=0, corner_radius=0, font=(LARGE, 25), width=75, height=45)
+            button.grid(row=row, column=column, sticky="nsew")
+            row += 1
 
     def run(self):
         self.geometry(self.center_window(400, 405, self._get_window_scaling()))
         self.create_display_frame()
         self.create_button_frame()
+        self.create_digit_buttons()
+        self.create_operator_buttons()
         self.mainloop()
 
 
