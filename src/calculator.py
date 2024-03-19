cc141ff7d3be45eebfca7f6dc4bd8f9145fc9f27
Martin Valapka <martin.valapka1@gmail.com>
2024-03-19 19:53:50 +0100

    File creation, header creation, color and font macros

---
+++ src/calculator.py
@@ -0,0 +1,19 @@
+"""
+@file calculator.py
+@brief File containing GUI of calculator application.
+
+@author
+- Martin Valapka (xvalapm00)
+@date March 19, 2024
+"""
+
+from customtkinter import CTk, CTkFrame, CTkButton, CTkLabel
+
+LIGHT_GRAY = "#979797"
+DARK_GRAY = "#3D3D3D"
+ORANGE = "#FFA500"
+GRAY = "#808080"
+COLOR_REST = "#4F4F4F"
+LABEL_COLOR = "#25265E"
+LARGE = "Arial 25 bold"
+SMALL = "Arial 15"
