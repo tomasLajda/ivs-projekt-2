a8bc0ef604181b83f6b1926c24d0b8a014f2a5c8
Martin Valapka <martin.valapka1@gmail.com>
2024-03-19 20:11:31 +0100

    Class creation with atributes, centered app window, main

--- src/calculator.py
+++ src/calculator.py
@@ -17,3 +17,27 @@ COLOR_REST = "#4F4F4F"
 LABEL_COLOR = "#25265E"
 LARGE = "Arial 25 bold"
 SMALL = "Arial 15"
+
+
+class App(CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Calculator")
+        self.resizable(False, False)
+        icon_path = r'ui\Pictures\Calculator_30001.ico'
+        self.iconbitmap(icon_path)
+
+    def center_window(self, width, height, scale_factor=1.0):
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = int(((screen_width / 2) - (width / 2)) * scale_factor)
+        y = int(((screen_height / 2) - (height / 2)) * scale_factor)
+        return f"{width}x{height}+{x}+{y}"
+
+    def run(self):
+        self.geometry(self.center_window(400, 405, self._get_window_scaling()))
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
