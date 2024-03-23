6b2526e2934654192b906ac0cbd5d12f3600dd32
VojtaGajda <gajda.vojta@gmail.com>
2024-03-23 20:45:53 +0100

    created mathlib, added +,-,*,/ functions

---
+++ src/mathlib.py
@@ -0,0 +1,63 @@
+"""
+@file mathlib.py
+@brief File containing math library.
+
+@author
+- Vojtěch Gajdušek (xgajduv00)
+- Martin Valapka (xvalapm00)
+@date March 23, 2024
+"""
+
+def add(num1, num2):
+    """
+    Function to add two numbers.
+    
+    Parameters:
+    num1: First number.
+    num2: Second number.
+    
+    Returns:
+    Sum of numbers num1 and num2.
+    """
+    return num1 + num2
+
+def sub(num1, num2):
+    """
+    Function to subtract two numbers.
+
+    Parameters:
+    num1: First number.
+    num2: Second number.
+    
+    Returns:
+    Difference of numbers num1 and num2 (num1 - num2).
+    """
+    return num1 - num2
+
+def mul(num1, num2):
+    """
+    Function to multiply two numbers.
+    
+    Parameters:
+    num1: First number.
+    num2: Second number.
+    
+    Returns:
+    Product of num1 and num2 (num1 * num2).
+    """
+    return num1 * num2
+
+def div(num1, num2):
+    """
+    Function to divide two numbers.
+    
+    Parameters:
+    num1: Divident.
+    num2: Divisor.
+    
+    Returns:
+    Quotient of num1 divided by num2 (num1 / num2).
+    """
+    if num2 == 0:
+        raise ValueError("Division by zero is not allowed.")
+    return num1 / num2
