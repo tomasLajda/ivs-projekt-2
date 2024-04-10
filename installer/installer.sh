b356b342bfd2f0ce3d93f3b5faa4f8c95bc46e2d
Matúš Csirik <matus.csirik@gmail.com>
2024-04-10 13:14:28 +0200

    changed to pyinstall

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# Check if the script is run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 mkdir -p Kalkulajda/DEBIAN
 mkdir -p Kalkulajda/usr/bin
 mkdir -p Kalkulajda/usr/local/bin
@@ -11,25 +18,22 @@ Architecture: all
 Depends: python3
 Maintainer: matus.csirik@gmail.com
 Description: Basic calculator." > Kalkulajda/DEBIAN/control
-cp calculator.py Kalkulajda/usr/bin/
-chmod +x Kalkulajda/usr/bin/calculator.py
-cp mathlib.py Kalkulajda/usr/bin/
-cp help_menu.py Kalkulajda/usr/bin/
+
 cp -r Pictures Kalkulajda/usr/bin/
-cp profiling.py Kalkulajda/usr/bin/
-chmod +x Kalkulajda/usr/bin/profiling.py
 
-# Create a shell script to run the Python script
-echo "#!/bin/sh
-python3 /usr/bin/calculator.py" > Kalkulajda/usr/bin/run_calculator.sh
-chmod +x Kalkulajda/usr/bin/run_calculator.sh
+# Copy the binary executables into the package
+cp calculator Kalkulajda/usr/bin/
+cp profiling Kalkulajda/usr/bin/
 
-# Create symbolic links to the shell script
-ln -sf /usr/bin/run_calculator.sh Kalkulajda/usr/local/bin/kalkulajda
+# Create symbolic links to the executables
+ln -sf /usr/bin/calculator Kalkulajda/usr/local/bin/kalkulajda
+ln -sf /usr/bin/profiling Kalkulajda/usr/local/bin/kalkulajda_p
 
 sudo apt-get -y update
 sudo apt-get -y install python3-pip
 sudo apt-get -y install python3-tk
 sudo apt-get -y install tk-dev
 pip3 install -r requirements.txt
+
 dpkg-deb --build Kalkulajda
+sudo dpkg -i Kalkulajda.deb
