b37cad205d10419b6842fa3c7d01b46914865089
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-11 16:43:19 +0200

    made a standard_deviation func which is then printed

--- src/profiling.py
+++ src/profiling.py
@@ -16,18 +16,29 @@ def seperate_numbers_from_std():
       print(f"Error: '{numberString}' isn't a number.")
       sys.exit()
 
-  if len(numbers) < 1000:
-    print("Error: The list must have at least 1000 numbers.")
-    sys.exit()
+  #if len(numbers) < 1000:
+  #  print("Error: The list must have at least 1000 numbers.")
+  #  sys.exit()
   
   return numbers
+
 def arithmetic_mean(numbers):
   sum = 0
   for number in numbers:
     sum = mathlib.add(sum, number)
 
-  return sum / len(numbers)
+  return mathlib.div(sum, len(numbers))
 
+def standard_deviation(numbers):
+  mean = arithmetic_mean(numbers)
+
+  sum = 0
+  for number in numbers:
+    sum = mathlib.add(sum, mathlib.pow(mathlib.sub(number, mean), 2))
+
+  result = mathlib.mul(sum, mathlib.div(1, mathlib.sub(len(numbers), 1)))
+
+  return mathlib.root(result, 2)
 
 numbers = seperate_numbers_from_std()
-arithmetic_mean(numbers)
+print(standard_deviation(numbers))
